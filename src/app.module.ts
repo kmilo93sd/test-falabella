228ef6a3dd80e8f8b1ed32a375eddbf7e08492bc
camilo@dill.cl-at-143347831018 <kmilo93sd@gmail.com>
2020-02-21 13:32:13 -0300

    added redis cache module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
+import { SalesController } from './controllers/sales/sales.controller';
 import { ProductsController } from './controllers/products/products.controller';
 import { MockProductsRepository } from './repositories/products/MockProductsRepository';
-import { SalesController } from './controllers/sales/sales.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
+  ],
   controllers: [ProductsController, SalesController],
   providers: [MockProductsRepository],
 })
